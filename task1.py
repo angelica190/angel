c86335da08182f1a89ef6c08c546843f7fd024e4
angelica190 <angelicadaniel1890@gmail.com>
2024-10-03 09:33:17 +0800

    task1.py

---
+++ task1.py
@@ -0,0 +1,9 @@
+list_of_fruits = ["banana", "apple", "orange", "mango"]
+
+for fruit in list_of_fruits:
+    if fruit == "banana":
+        print("hello banana")
+    elif fruit == "apple":
+        print("hello apple")
+    else:
+        print("no")
